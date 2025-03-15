cab5c083814088ea5ee5ed65c52cfe76abdff67e
sawmeji <sawmejisan@gmail.com>
2025-03-15 21:29:00 +0700

    Changing df file name

--- cities_join_exe.py
+++ cities_join_exe.py
@@ -1,32 +1,32 @@
 import pandas as pd
 import requests 
 
-url = "https://raw.githubusercontent.com/owid/covid-19-data/refs/heads/master/public/data/latest/owid-covid-latest.json"
+covid19_url = "https://raw.githubusercontent.com/owid/covid-19-data/refs/heads/master/public/data/latest/owid-covid-latest.json"
 
 cities_url = "https://raw.githubusercontent.com/dr5hn/countries-states-cities-database/refs/heads/master/json/cities.json"
 
-raw_data = requests.get(url).json()
+countries_raw_data = requests.get(covid19_url).json()
 
-city_raw_data = requests.get(cities_url).json()
+cities_raw_data = requests.get(cities_url).json()
 
-cities = pd.DataFrame(city_raw_data)
+cities_df = pd.DataFrame(cities_raw_data)
 
-all_countries = pd.DataFrame()
+all_countrie_df = pd.DataFrame()
 
-for country_short_code in raw_data.keys():
-    single_country_df = pd.json_normalize(raw_data[country_short_code])
+for country_short_code in countries_raw_data.keys():
+    single_country_df = pd.json_normalize(countries_raw_data[country_short_code])
     single_country_df['iso_country_code'] = country_short_code
-    all_countries = pd.concat([all_countries, single_country_df], ignore_index=True)
+    all_countrie_df = pd.concat([all_countrie_df, single_country_df], ignore_index=True)
 
-print(all_countries.head())
-print(all_countries.columns)
-print(all_countries.shape)
+print(all_countrie_df.head())
+print(all_countrie_df.columns)
+print(all_countrie_df.shape)
 
-print(cities.head())
-print(cities.columns)
-print(cities.shape)
+print(cities_df.head())
+print(cities_df.columns)
+print(cities_df.shape)
 
-cities_join_data = pd.merge(cities, all_countries, left_on='country_name', right_on='location', how='inner')
+cities_join_data = pd.merge(cities_df, all_countrie_df, left_on='country_name', right_on='location', how='inner')
 
 print(cities_join_data.head())
 print(cities_join_data.columns)
